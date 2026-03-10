62629ec8fb66512c8f4cebccd585f21801587eba
Smyku6 <smyku6@gmail.com>
2026-03-10 23:53:02 +0100

    admin update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Redirect /admin/ to /admin/index.html
+    {
+      name: 'admin-redirect',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/photos/admin' || req.url === '/photos/admin/') {
+            req.url = '/photos/admin/index.html'
+          }
+          next()
+        })
+      }
+    }
+  ],
   base: '/photos/', // GitHub repo name - change if different
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false
-  }
+  },
+  // Ensure public files are copied as-is
+  publicDir: 'public'
 })
